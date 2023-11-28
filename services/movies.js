364c75b5c833a49159df4057a462b7a19564e4ac
samuelSoler99 <samuelsolerpico99@gmail.com>
2023-11-28 20:51:19 +0100

    first commit

--- services/movies.js
+++ services/movies.js
@@ -1,20 +1,20 @@
-const API_KEY = 'b77d3f11';
-export const searchMoves = async ({search}) => {
-    if (search === '')return null
+const API_KEY = 'b77d3f11'
+export const searchMoves = async ({ search }) => {
+  if (search === '') return null
 
-    try {
-        const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
-        const json = await response.json()
+  try {
+    const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
+    const json = await response.json()
 
-        const movies = json.Search
+    const movies = json.Search
 
-        return  movies?.map(movie => ({
-            id: movie.imdbID,
-            title: movie.Title,
-            year: movie.Year,
-            poster: movie.Poster
-        }))
-    } catch (e){
-        throw new Error('Error searching movies')
-    }
+    return movies?.map(movie => ({
+      id: movie.imdbID,
+      title: movie.Title,
+      year: movie.Year,
+      poster: movie.Poster
+    }))
+  } catch (e) {
+    throw new Error('Error searching movies')
+  }
 }
